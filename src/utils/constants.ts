48ad4638bc6b39adff4e5ba1132dcf5647b613ae
Khalid Alansary <khaled.ansary2003@outlook.com>
2025-06-21 19:46:54 +0300

    feat: create xdgGlobalKeybinds plugin

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -629,6 +629,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "prism",
         id: 390884143749136386n,
     },
+    khald0r: {
+        name: "khald0r",
+        id: 294752882253889546n
+    },
 } satisfies Record<string, Dev>);
 
 // iife so #__PURE__ works correctly
